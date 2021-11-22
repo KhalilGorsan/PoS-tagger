65a45fcb911f18a588821e817bc2233888e94c02
GorsanMestiri <khalilgorsan@gmail.com>
2021-11-22 22:26:50 +0100

    feat(model): Save and load the model from pkl + some refactoring

--- generate.py
+++ generate.py
@@ -1,14 +1,24 @@
 """Script to generate Pos tags for a given text input"""
+import pickle
+
 from data.preprocessor import extract_features_from_text
-from train import crf_model
-
-input = "The weather is great !"
-input_as_list = input.split()
+from train import MODEL_PATH
 
 
-input_as_feat = [
-    extract_features_from_text(input_as_list, i) for i in range(len(input_as_list))
-]
+def main():
+    input = "The weather is great !"
+    input_as_list = input.split()
 
-pred_tags = crf_model.predict_single(input_as_feat)
-print(f"The associated pos tags of {input} are {pred_tags}")
+    with open(MODEL_PATH, "rb") as f:
+        crf_model = pickle.load(f)
+
+    input_as_feat = [
+        extract_features_from_text(input_as_list, i) for i in range(len(input_as_list))
+    ]
+
+    pred_tags = crf_model.predict_single(input_as_feat)
+    print(f"The associated pos tags of {input} are {pred_tags}")
+
+
+if __name__ == "__main__":
+    main()
--- train.py
+++ train.py
@@ -1,20 +1,34 @@
 """Training script"""
+import pickle
+from pathlib import Path
+
 from sklearn_crfsuite import CRF
 
 from data.preprocessor import make_it_dataset, ud_corpus_as_list_of_tokens
 
-TRAIN_DATA_PATH = "data/english-GUM/en_gum-ud-train.conllu.txt"
-DEV_DATA_PATH = "data/english-GUM/en_gum-ud-dev.conllu.txt"
+TRAIN_DATA_PATH = Path("data", "english-GUM", "en_gum-ud-train.conllu.txt")
+DEV_DATA_PATH = Path("data", "english-GUM", "en_gum-ud-dev.conllu.txt")
+MODEL_PATH = Path("models", "crf_model.sav")
 
 # Read the train input data and make a dataset
 train_data = open(TRAIN_DATA_PATH)
 list_of_tokens = ud_corpus_as_list_of_tokens(train_data)
 x_train, y_train = make_it_dataset(list_of_tokens)
 
-crf_model = CRF(
-    "lbfgs", c1=0.01, c2=0.1, max_iterations=100, all_possible_transitions=True
-)
 
-print("Start training for the Pos tagging task on the UD-GUM corpus")
-crf_model.fit(x_train, y_train)
-print("Finished training on the UD-GUM corpus")
+def main():
+    crf_model = CRF(
+        "lbfgs", c1=0.01, c2=0.1, max_iterations=100, all_possible_transitions=True
+    )
+
+    print("Start training for the Pos tagging task on the UD-GUM corpus")
+    crf_model.fit(x_train, y_train)
+    print("Finished training on the UD-GUM corpus")
+
+    # save the mode to disk
+    with open(MODEL_PATH, "wb") as f:
+        pickle.dump(crf_model, f)
+
+
+if __name__ == "__main__":
+    main()
